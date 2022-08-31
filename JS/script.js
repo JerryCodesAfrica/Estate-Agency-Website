1025ec11317705af55f26beb5a82325f031a1b4b
Jerryking18 <egemonyeemelie@gmail.com>
2022-08-31 22:15:38 +1000

    update 31/08/22

--- JS/script.js
+++ JS/script.js
@@ -1,23 +1,30 @@
-// Carousel functionality
-
-var slides = document.querySelectorAll('.slide');
-var btns = document.querySelectorAll('.btn')
-let currentSlide = 1;
-//
-//manual navigation
-var manualNav = function(manual){
-    slides.forEach((slide)=>{
-        slides.classList.remove('active');
-        btns.forEach((slide)=>{
-            btns.classList.remove('active');
-        });
-    });
-    slides[manual].classList.add('active');
-    btns[manual].classList.add('active');
-}
-
-
-btns.forEach((btn, i)=>{
-   manualNav(i);
-   currentSlide = i; 
-})
+// Latest properties slide
+// Latest-properties
+ var button1 = document.querySelector(".ps1");
+ var button2 = document.querySelector(".ps2");
+ var slidepageleft = document.querySelector(".p1");
+ var slidepageright = document.querySelector(".p4p");
+ function slideleft(){
+    button2.classList.remove("ps2");
+    button2.classList.add("active");
+    button1.classList.add("ps2");
+    slidepageleft.style.width = "0";
+    slidepageright.style.width = "720px"
+    //  slidepageleft.style.display = "none";
+     slidepageright.style.display = "inline";
+    
+ } 
+ button2.addEventListener('click',slideleft);
+//  
+function slideright(){
+    button1.classList.remove("ps2");
+    button1.classList.add("active");
+    button2.classList.add("ps2");
+    slidepageleft.style.width = "720px";
+    slidepageright.style.width = "0";
+    //  slidepageleft.style.display = "none";
+     slidepageright.style.display = "none";
+    
+ } 
+ button1.addEventListener('click',slideright);
+// 
